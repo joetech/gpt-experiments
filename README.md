97a0337bc69d404fc788ec3bd1f162fb02498212
Joe Colburn <joe@joetech.com>
2023-06-29 02:23:32 -0400

    hangman example

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ I experiment with ChatGPT quite a bit, so I though I'd share some of the prompts
 Each experiment has its own directory and inside will be all the files for the working experiment as well as a *chat.html* file that includes all the prompts and responses.  Save and open the chat.html file to view it.
 
 ## Experiments
-- [Rock, Paper, Scissors game](experiments/rps/rps.html) / [chat history](experiments/rps/chat.html)
+- [Rock, Paper, Scissors game](experiments/rps/) / [chat history](experiments/rps/chat.html)
+- [Hangman game](experiments/hangman/) / [chat history](experiments/hangman/chat.html)
 
 
 
---
+++ experiments/hangman/hangman.js
@@ -0,0 +1,175 @@
+let selectedWord; // Word to be guessed
+let guessedLetters = []; // Guessed letters
+let remainingGuesses; // Remaining guesses
+
+const wordElement = document.getElementById("word");
+const guessInput = document.getElementById("guess");
+const submitButton = document.getElementById("submit");
+const statusElement = document.getElementById("status");
+const playAgainButton = document.getElementById("play-again");
+const guessedLettersElement = document.getElementById("guessed-letters");
+const hangmanArtElement = document.getElementById("hangman-art");
+const hangmanParts = [
+  `
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+`,
+  `
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+`,
+  `
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+`,
+  `
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+`,
+  `
+  +---+
+  |   |
+  O   |
+ /|\\  |
+      |
+      |
+`,
+  `
+  +---+
+  |   |
+  O   |
+ /|\\  |
+ /    |
+      |
+`,
+  `
+  +---+
+  |   |
+  O   |
+ /|\\  |
+ / \\  |
+      |
+`,
+];
+
+let wrongGuesses = 0; // Counter for wrong guesses
+
+
+// Function to update the hangman art
+function updateHangmanArt() {
+  hangmanArtElement.textContent = hangmanParts[wrongGuesses];
+}
+
+// Update the hangman art and check the game status after each guess
+function guessLetter() {
+  const letter = guessInput.value.toLowerCase();
+  guessInput.value = "";
+
+  if (letter && !guessedLetters.includes(letter)) {
+    guessedLetters.push(letter);
+
+    if (!selectedWord.includes(letter)) {
+      wrongGuesses++;
+      if (wrongGuesses >= hangmanParts.length) {
+        statusElement.innerHTML = `Game Over! The word was "${selectedWord}".`;
+        submitButton.disabled = true;
+        playAgainButton.style.display = "inline";
+        return;
+      }
+    }
+    remainingGuesses--;
+
+    wordElement.innerHTML = getHiddenWord();
+    updateHangmanArt();
+    updateGuessedLetters();
+    checkGameStatus();
+
+    // Disable the guess form after the game is lost
+    if (remainingGuesses === 0) {
+      guessInput.disabled = true;
+      submitButton.disabled = true;
+      return;
+    }
+  }
+}
+
+// Initialize the game
+function initializeGame() {
+  selectedWord = wordList[Math.floor(Math.random() * wordList.length)];
+  remainingGuesses = hangmanParts.length;
+  guessedLetters = [];
+  wrongGuesses = 0;
+  wordElement.innerHTML = getHiddenWord();
+  guessInput.value = "";
+  statusElement.innerHTML = "";
+  playAgainButton.style.display = "none";
+  submitButton.disabled = false;
+  updateHangmanArt();
+  updateGuessedLetters();
+
+  // Set focus to the guess form element
+  guessInput.disabled = false;
+  guessInput.focus();
+}
+
+// Function to update the guessed letters display
+function updateGuessedLetters() {
+  guessedLettersElement.textContent = "Guessed Letters: " + guessedLetters.join(", ");
+}
+
+// Generate the hidden word with underscores for unguessed letters
+function getHiddenWord() {
+  let hiddenWord = "";
+  for (let i = 0; i < selectedWord.length; i++) {
+    if (guessedLetters.includes(selectedWord[i])) {
+      hiddenWord += selectedWord[i];
+    } else {
+      hiddenWord += "_";
+    }
+    hiddenWord += " ";
+  }
+  return hiddenWord.trim();
+}
+
+// Check the game status (win, lose, or continue)
+function checkGameStatus() {
+  console.log('remainingGuesses', remainingGuesses);
+  console.log('wrongGuesses', wrongGuesses);
+  if (remainingGuesses === 0) {
+    statusElement.innerHTML = `Game Over! The word was "${selectedWord}".`;
+    submitButton.disabled = true;
+    playAgainButton.style.display = "inline";
+  } else if (!getHiddenWord().includes("_")) {
+    statusElement.innerHTML = "Congratulations! You guessed the word!";
+    submitButton.disabled = true;
+    playAgainButton.style.display = "inline";
+  }
+}
+
+// Event listeners
+submitButton.addEventListener("click", guessLetter);
+guessInput.addEventListener("keyup", function(event) {
+  if (event.key === "Enter") {
+    guessLetter();
+  }
+});
+playAgainButton.addEventListener("click", initializeGame);
+
+// Start the game
+initializeGame();
